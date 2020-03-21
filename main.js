46dd470d13003705f1bdf367fcfa4f6c9304ece7
Yosra <yos.skhiri@gmail.com>
2020-03-21 20:54:14 +0100

    added the side menu

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
 let hamburger = document.querySelector('#hamburger');
 let menu = document.querySelector('.nav-links');
-
-console.log(hamburger.children[0].children)
+let menuOut = true;
 
 hamburger.addEventListener('click', (e)=>{
-    if(menu.style.display === "none"){
-        menu.style.display = "flex"
-        hamburger.style.fill = '#fff';
-        hamburger.children[0].children[0].style.stroke="#fff"
-        hamburger.children[0].children[1].style.stroke="#fff"
-        hamburger.children[0].children[2].style.stroke="#fff"
+    if(!menuOut){
+        menu.style.display = "flex";
+        menu.style.animation = "menu-slide-in .3s ease-in forwards";
+        for(i=0; i < 3; i++){
+            hamburger.children[0].children[i].style.stroke="#fff";
+        }
+        menuOut = true
       
     }else{
-        menu.style.display = "none"
-        hamburger.children[0].children[0].style.stroke="#707070"
-        hamburger.children[0].children[1].style.stroke="#707070"
-        hamburger.children[0].children[2].style.stroke="#707070"
+        menu.style.animation = "menu-slide-out .3s ease-out forwards";
+        for(i=0; i < 3; i++){
+            hamburger.children[0].children[i].style.stroke="#000";
+        }
+        menuOut = false
     }
 });
 
