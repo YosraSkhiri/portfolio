e40bccba6e5f4afa421da810048e18b2dd21c9f9
Yosra <yos.skhiri@gmail.com>
2020-03-24 22:06:32 +0100

    making progress on the about page + added the javascript logic

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 let hamburger = document.querySelector('#hamburger');
 let menu = document.querySelector('.nav-links');
+let edu = document.querySelector('#edu');
+let job = document.querySelector('#job');
+
+console.log()
+
+let eduContent = document.querySelector('#edu-content');
+let jobContent = document.querySelector('#job-content');
 let menuOut = false;
+let showedu = true;
 
 hamburger.addEventListener('click', function() {
   if(!menuOut){
@@ -12,5 +20,25 @@ hamburger.addEventListener('click', function() {
   }
     this.classList.toggle('active');
   });
+
+if(document.documentElement.clientWidth <= 768){
+  jobContent.style.display = "none"
+  edu.addEventListener('click', function() {
+    if(!showedu){
+      eduContent.style.display = "block";
+      jobContent.style.display = "none"
+      showedu = true;
+    }
+  })
+  
+  job.addEventListener('click', function() {
+    if(showedu){
+      eduContent.style.display = "none";
+      jobContent.style.display = "block"
+      showedu = false;
+    }
+  })
+}
+
 
 
